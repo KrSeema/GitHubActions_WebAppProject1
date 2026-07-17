9fd12fe2dde3e1c0e12eefab0b90fbe81d81c4fc
simmikr <simmikanwar@gmail.com>
2026-07-17 07:22:59 +0000

    V2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// If you want to start measuring performance in your app, pass a function
+// This is Important
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
